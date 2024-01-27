8c1096600897980afffae290ad48f721cb3e189f
LeeJincheon <pressky99@gmail.com>
2024-01-28 02:38:46 +0900

    feat: 화면 및 컨트롤러 작성

--- src/main/java/blog/example/blogspringboot/domain/Article.java
+++ src/main/java/blog/example/blogspringboot/domain/Article.java
@@ -4,24 +4,37 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-@Entity // 엔티티로 지정
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
-    @Id // id 필드를 기본키로 지정
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
-    @Column(name = "id", updatable = false) // id 컬럼과 매핑
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "title", nullable = false) // title 컬럼과 매핑 (not null)
+    @Column(name = "title", nullable = false)
     private String title;
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder
     public Article(String title, String content) {
---
+++ src/main/resources/query.sql
@@ -0,0 +1,2 @@
+insert into article (title, content, created_at, updated_at) values ('title@', 'content@', now(), now());
+select * from article;
