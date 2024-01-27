4aa416a54c8a3055d08c02a18f76b179faea7113
LeeJincheon <pressky99@gmail.com>
2024-01-28 03:36:07 +0900

    User entity, repository, service 작성

---
+++ src/main/java/blog/example/blogspringboot/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package blog.example.blogspringboot.service;
+
+import blog.example.blogspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override // 사용자 이름(email)으로 사용자 정보를 사져오는 메서드
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
